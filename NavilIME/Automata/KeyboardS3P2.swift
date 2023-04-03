9a54e077b99a3a77b0cb8022a6916851cbdd8e76
ImBada <imbada@outlook.com>
2023-04-04 08:45:46 +0900

    신세벌식 P2 배열 추가. (테스트 완료)
    개인적으로 아래아를 사용하지 않아서 관련 기능이 정상 작동하는지는 테스트하지 못하였으나 현대 한글 입력에는 전혀 문제가 없음을 테스트하였습니다. 최대한 원본 배열과 동일한 경험을 줄 수 있도록 만들었습니다.

---
+++ NavilIME/Automata/KeyboardS3P2.swift
@@ -0,0 +1,129 @@
+//
+//  KeyboardS3P2.swift
+//  NavilIME
+//
+//  Created by BADA IM on 2023/04/04.
+//
+
+import Foundation
+
+class KeyboardS3P2 : Keyboard {
+    override init() {
+        super.init()
+        
+        self.name = "신세벌P2"
+        self.id = 32
+        
+        // 초성 레이아웃
+        self.chosung_layout = [
+            "/":Chosung.Kiyuk,
+            
+            "y":Chosung.Riel,
+            "u":Chosung.Digek,  "uu":Chosung.SsDigek,
+            "i":Chosung.Miem,
+            "o":Chosung.Chiek,
+            "p":Chosung.Piep,
+            
+            "h":Chosung.Nien,
+            "j":Chosung.Yieng,
+            "k":Chosung.Giyuk,  "kk":Chosung.SsGiyuk,
+            "l":Chosung.Jiek,   "ll":Chosung.SsJiek,
+            ";":Chosung.Biep,   ";;":Chosung.SsBiep,
+            "'":Chosung.Tigek,
+            
+            "n":Chosung.Siot,   "nn":Chosung.SsSiot,
+            "m":Chosung.Hiek
+        ]
+        
+        // 중성 레이아웃
+        self.jungsung_layout = [
+            "q":Jungsung.Yae,
+            "w":Jungsung.Ya,
+            "e":Jungsung.Ae,
+            "r":Jungsung.Eo,
+            "t":Jungsung.Yeo,
+            
+            "a":Jungsung.Yu,
+            "s":Jungsung.Ye,
+            "d":Jungsung.I,
+            "f":Jungsung.A,
+            "g":Jungsung.Eu,
+            
+            "z":Jungsung.YetAraea,
+            
+            "x":Jungsung.Yo,
+            "c":Jungsung.E,
+            "v":Jungsung.O,
+            "b":Jungsung.U,
+            
+            //Jungsung.Yi ㅢ는 신세벌P2에서 사용되지 않음
+            "o":Jungsung.U,
+            "/":Jungsung.O,
+            
+            // 이중모음
+            "vf":Jungsung.Wa,       "/f":Jungsung.Wa,
+            "ve":Jungsung.Wae,      "/e":Jungsung.Wae,
+            "vd":Jungsung.Oe,       "/d":Jungsung.Oe,
+            "br":Jungsung.Weo,      "or":Jungsung.Weo,
+            "bc":Jungsung.We,       "oc":Jungsung.We,
+            "bd":Jungsung.Wi,       "od":Jungsung.Wi
+        ]
+        
+        // 종성 레이아웃
+        self.jongsung_layout = [
+            "q":Jongsung.Sios,
+            "w":Jongsung.Rieul,
+            "e":Jongsung.Pieup,
+            "r":Jongsung.Thieuth,
+            "t":Jongsung.Khieukh,
+            
+            "a":Jongsung.Ieung,
+            "s":Jongsung.Nieun,
+            "d":Jongsung.Hieuh,
+            "f":Jongsung.Phieuph,
+            "g":Jongsung.Tikeut,
+            
+            "z":Jongsung.Mieum,
+            "x":Jongsung.Ssangsios,
+            "c":Jongsung.Kiyeok,
+            "v":Jongsung.Cieuc,
+            "b":Jongsung.Chieuch,
+            
+            // 겹받침
+            "sd":Jongsung.Nieunhieuh,  // ᆭ
+            "wc":Jongsung.Rieulkiyeok, // ᆰ
+            "cc":Jongsung.Ssangkiyeok, // ᆩ
+            "eq":Jongsung.Pieupsios,   // ᆹ
+            "wz":Jongsung.Rieulmieum,  // ᆱ
+            "wd":Jongsung.Rieulhieuh,  // ᆶ
+            "cq":Jongsung.Kiyeoksios,  // ᆪ
+            "sv":Jongsung.Nieuncieuc,  // ᆬ
+            "we":Jongsung.Rieulpieup,  // ᆲ
+            "wq":Jongsung.Rieulsios,   // ᆳ
+            "wr":Jongsung.Rieulthieuth,// ᆴ
+            "wf":Jongsung.Rieulphieuph,// ᆵ
+        ]
+        
+        // 기타 기호, 숫자 레이아웃
+        self.etc_layout = [
+            "Y":"×", "U":"○", "I":"I", "O":"O", "P":";",
+            "H":"□", "J":"'", "K":"\"", "L":"·", ":":":", "\"":"/",
+            "N":"―", "M":"…", "<":"<", ">":">", "?":"?",
+        ]
+    }
+    
+    override func chosung_proc(comp: inout Composition, ch: String) -> Bool {
+        let chokey:String = comp.chosung + ch
+        return self.chosung_layout[chokey] != nil ? true : false
+    }
+    
+    override func jungsung_proc(comp: inout Composition, ch: String) -> Bool {
+        let jungkey:String = comp.jungsung + ch
+        return self.jungsung_layout[jungkey] != nil ? true : false
+    }
+    
+    override func jongsung_proc(comp: inout Composition, ch: String) -> Bool {
+        let jongkey:String = comp.jongsung + ch
+        return self.jongsung_layout[jongkey] != nil ? true : false
+    }
+}
