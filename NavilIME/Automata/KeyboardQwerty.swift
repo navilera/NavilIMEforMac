66e12863c6976c400505ecc52045a496c4dec983
imbada <imbada@outlook.com>
2024-04-26 07:07:06 +0900

    지우자

---
+++ NavilIME/Automata/KeyboardQwerty.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardS3P2.swift
+//  NavilIME
+//
+//  Created by BADA IM on 2023/04/04.
+//
+
+import Foundation
+
+class KeyboardQwerty : Keyboard {
+    override init() {
+        super.init()
+        
+        self.name = "영어 QWERTY"
+        self.id = 99
+    }
+}
